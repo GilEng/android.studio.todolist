f0e631b1ef24dbd97c1b598f9fcac71776eb4e0a
Thiago Porto <tporto88@gmail.com>
2014-10-02 00:24:42 -0300

    First commit

---
+++ app/src/main/java/dao/UsuarioDAO.java
@@ -0,0 +1,43 @@
+package dao;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import model.Usuario;
+
+public class UsuarioDAO {
+
+    private DatabaseHelper databaseHelper;
+    private SQLiteDatabase database;
+
+    public UsuarioDAO(Context context){
+        databaseHelper = new DatabaseHelper(context);
+    }
+
+    private SQLiteDatabase getDatabase(){
+        if (database == null){
+            database = databaseHelper.getWritableDatabase();
+        }
+
+        return database;
+    }
+
+    private Usuario criarUsuario(Cursor cursor){
+        Usuario model = new Usuario(
+            cursor.getInt(cursor.getColumnIndex(DatabaseHelper.Usuarios._ID)),
+            cursor.getString(cursor.getColumnIndex(DatabaseHelper.Usuarios.NOME)),
+            cursor.getString(cursor.getColumnIndex(DatabaseHelper.Usuarios.LOGIN)),
+            cursor.getString(cursor.getColumnIndex(DatabaseHelper.Usuarios.SENHA))
+        );
+
+        return model;
+    }
+
+    public void fechar(){
+        databaseHelper.close();
+        database = null;
+    }
+
+
+}
