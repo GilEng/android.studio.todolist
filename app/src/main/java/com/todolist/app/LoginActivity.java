35013ad0e7e7318593eae167b134742bb81f37e8
Thiago Porto <tporto88@gmail.com>
2014-10-05 00:39:56 -0300

    Finalizado TarefaDAO e implementado o logar

--- app/src/main/java/com/todolist/app/LoginActivity.java
+++ app/src/main/java/com/todolist/app/LoginActivity.java
@@ -1,19 +1,56 @@
 package com.todolist.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import dao.UsuarioDAO;
 
 
 public class LoginActivity extends ActionBarActivity {
+
+    private EditText edtUsuario, edtSenha;
+    private UsuarioDAO helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtUsuario = (EditText) findViewById(R.id.login_edtUsuario);
+        edtSenha   = (EditText) findViewById(R.id.login_edtSenha);
+
+        helper = new UsuarioDAO(this);
     }
 
+    public void logar(View view){
+        String usuario = edtUsuario.getText().toString();
+        String senha   = edtSenha.getText().toString();
+
+        boolean validacao = true;
+
+        if(usuario == null || usuario.equals("")){
+            validacao = false;
+            edtUsuario.setError(getString(R.string.login_valUsuario));
+        }
+
+        if(senha == null || senha.equals("")){
+            validacao = false;
+            edtSenha.setError(getString(R.string.login_valSenha));
+        }
+
+        if(validacao){
+            //logar
+            if(helper.logar(usuario,senha)){
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
