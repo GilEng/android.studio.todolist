ff0cd1049ee403735e87e057ab4f062e3dbb29b4
Thiago Porto <tporto88@gmail.com>
2015-04-15 23:15:09 -0300

    logout

--- app/src/main/java/com/todolist/app/MainActivity.java
+++ app/src/main/java/com/todolist/app/MainActivity.java
@@ -1,10 +1,15 @@
 package com.todolist.app;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import util.Mensagem;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,11 +29,28 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
 
-        if (id == R.id.action_lista_usuarios) {
-            startActivity(new Intent(this, ListUsuariosActivity.class));
+        switch (item.getItemId()){
+            case R.id.action_lista_usuarios:
+                startActivity(new Intent(this, ListUsuariosActivity.class));
+                break;
+            case R.id.action_lista_sair:
+                Mensagem.MsgConfirm(this, "Sair do sistema", "Deseja realmente sair?", R.drawable.ic_action_about, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                break;
+            case R.id.action_lista_logout:
+                SharedPreferences preferences   = getSharedPreferences("LoginActivityPreferences", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.clear();
+                editor.commit();
+                finish();
+                break;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
